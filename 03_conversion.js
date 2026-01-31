47977906fbb40b8dbf9a49bb9a98cf9694fe6e75
25mcmc30-Akshay guru <25mcmc30@uohyd.ac.in>
2026-01-31 19:50:22 +0530

    Add files via upload

---
+++ 03_conversion.js
@@ -0,0 +1,29 @@
+let score = null
+// console.log(typeof score);
+let valueIn = Number(score); // this will convert the datatype to number
+console.log(typeof valueIn); // this conatains the datatype of nuber
+console.log( score); // output = NaN not a number
+
+//converting to boolean data type
+let isfollow ="efrg"
+let convertbool = Boolean(isfollow)   
+console.log(typeof convertbool); 
+console.log(typeof isfollow);  
+console.log( convertbool);  
+
+
+
+//Note : here isfollow contains the number datatype 
+// and using Boolean we converted the number to boolean datatype
+
+// 1= true : 0 = false
+// " "= false
+//" hitesh " = true
+
+//converting to string
+let firstname = "33"
+let convertstring = String(firstname)
+console.log(firstname);
+console.log(typeof convertstring);
+console.log(convertstring);
+
