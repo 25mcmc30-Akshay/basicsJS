27051eb74add2835bc18918a8240af01637a3316
25mcmc30-Akshay guru <25mcmc30@uohyd.ac.in>
2026-01-31 19:49:39 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,29 @@
+console.log("hello");
+const account=12334;
+// account=120;
+let accountEmail = "hari"
+city = "hyderabad"
+password  ="1234"
+let accountState;// will give u undefined 
+console.table([account,accountEmail,password,city,accountState])
+console.log(account);
+console.log(accountState);
+
+
+
+
+
+
+
+
+// Non-primitive = can store multiple values or complex structures
+
+// Stored in heap memory, variable holds a reference (address).
+
+// Types
+// Type	Example
+// Object	{ name: "Akshay" }
+// Array	[1, 2, 3]
+// Function	function add(){}
+// Date	new Date()
+// Map/Set	new Map()
