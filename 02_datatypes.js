1043cc99c89dd87175faa5031c10d0fe24f38474
25mcmc30-Akshay guru <25mcmc30@uohyd.ac.in>
2026-01-31 19:50:05 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,29 @@
+let name ="alaska"
+let age =19
+let isfollow = true
+
+//number = 2^50 
+//bigint
+//string =" 
+// boolean = true false"
+// null = standalone
+// undefined 
+// symbol -> unique
+
+
+//object 
+console.log(typeof"alaska") // output willbe string
+console.log(typeof age)  // output will be age 
+console.log(typeof isfollow) // output will be boolean
+console.log(typeof null)  //**********/ output will be object 
+
+// null is reported as an object due to a historical bug in JavaScript’s type system. It is not actually an object, 
+// but the behavior is preserved for backward compatibility.
+
+// let x = null;
+// Two things happen:
+// Memory is allocated for variable x;
+// The value stored in that memory is null;
+
+console.log(typeof undefined)  //output will be undefined 
+
